6013aff4eb0aa2527a540bb92e39d81349c7d152
kisze <kiszel1dani@gmail.com>
2025-03-03 12:16:07 +0100

    Üdvözlő lap elkészítése(2 gomb , szöveg, image-slider) // Creating the welcome screen(2 buttons, text, image-slider)

--- app/src/main/java/com/example/kiszeldaniel_pcjkbh/MainActivity.java
+++ app/src/main/java/com/example/kiszeldaniel_pcjkbh/MainActivity.java
@@ -1,32 +1,53 @@
 package com.example.kiszeldaniel_pcjkbh;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.kiszeldaniel_pcjkbh.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private final Handler handler = new Handler();
+    private Runnable runnable;
+    private int currentPage = 0;
+    private final int[] images = {R.drawable.bb1, R.drawable.bb2, R.drawable.bb3, R.drawable.bb4, R.drawable.bb5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager2 mozgoKep = findViewById(R.id.mozgoKep);
+
+        MozgoKep adapter = new MozgoKep(new int[]{R.drawable.bb1, R.drawable.bb2, R.drawable.bb3, R.drawable.bb4, R.drawable.bb5});
+        mozgoKep.setAdapter(adapter);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage == images.length) {
+                    currentPage = 0;
+                }
+                mozgoKep.setCurrentItem(currentPage++, true);
+                handler.postDelayed(this, 3000);
+            }
+        };
+
+        handler.postDelayed(runnable, 3000);
+
+        mozgoKep.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+            }
+        });
+
     }
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            handler.removeCallbacks(runnable);
+        }
+
 
 }
---
+++ app/src/main/java/com/example/kiszeldaniel_pcjkbh/MozgoKep.java
@@ -0,0 +1,41 @@
+package com.example.kiszeldaniel_pcjkbh;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+public class MozgoKep extends RecyclerView.Adapter<MozgoKep.ViewHolder> {
+    private final int[] images;
+
+    public MozgoKep(int[] images) {
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mainxml_imagine_slider, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.mozgoKep);
+        }
+    }
+}
